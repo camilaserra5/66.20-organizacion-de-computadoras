d9e3e1f3c8d5576abc79182a8ecc0bee8f454f4c
Cynthia Gamarra <cgamarra@fi.uba.ar>
2020-11-11 02:08:05 -0300

    Código completo en mips. Falta probarlo para ver que surjan errores. Tengo dudas con algunos branch

--- tp1/mcd.s
+++ tp1/mcd.s
@@ -1,63 +1,143 @@
-000011a8 <_mcd>:
+#include <sys/regdef.h>
 
-// máximo común divisor
-int _mcd(int m, int n) {
-lui	gp,0x2
-addiu	gp,gp,-28984
-addu	gp,gp,t9
-addiu	sp,sp,-32
-sw	ra,28(sp)
-sw	s8,24(sp)
-move	s8,sp
-sw	gp,16(sp)
-sw	a0,32(s8)
-sw	a1,36(s8)
-if (m == 0)
-lw	v0,32(s8)
-bnez	v0,11e8 <_mcd+0x40>
+.text
+.abicalls
+.align  2
+.globl  mcd
+.ent  mcd
 
-return m;
-lw	v0,32(s8)
-b	1288 <_mcd+0xe0>
+# Pipeline magic.
+.set        noreorder
+.cpload     t9
+.set        reorder
 
-if (n == 0)
-lw	v0,36(s8)
-bnez	v0,1200 <_mcd+0x58>
-
-return n;
-lw	v0,36(s8)
-b	1288 <_mcd+0xe0>
-
-
-if (m == n)
-lw	v1,32(s8)
-lw	v0,36(s8)
-bne	v1,v0,121c <_mcd+0x74>
-nop
-return m;
-lw	v0,32(s8)
-b	1288 <_mcd+0xe0>
+/* STACK DESIGN - SIZE: 32
+      ==========
+  36  |   a1   |
+      ---------- ABA CALLER (8)
+  32  |   a0   |
+      ==========
+  28  | PADDING|
+      ----------
+  24  |   ra   |
+      ---------- SRA (16)
+  20  |   gp   |
+      ----------
+  16  |   fp   |
+      ==========
+  12  |   a3   |
+      ----------
+  8   |   a2   |
+      ---------- ABA CALLEE(16)
+  4   |   a1   |
+      ----------
+  0   |   a0   |
+      ==========
+*/
 
 
-if (m > n)
-lw	v1,32(s8)
-lw	v0,36(s8)
-slt	v0,v0,v1
-beqz	v0,1260 <_mcd+0xb8>
-return _mcd(m - n, n);
-lw	v1,32(s8)
-lw	v0,36(s8)
-subu	v0,v1,v0
-lw	a1,36(s8)
-move	a0,v0
-lw	v0,-32704(gp)
-move	t9,v0
-bal	11a8 <_mcd>
-lw	gp,16(s8)
-b	1288 <_mcd+0xe0>
-nop
-return _mcd(m, n - m);
-lw	v1,36(s8)
-lw	v0,32(s8)
-subu	v0,v1,v0
-move	a1,v0
+#  Argument-building area callee (ABA callee).
+#define A0_CALLEE 0
+#define A1_CALLEE 4
+#define A2_CALLEE 8
+#define A3_CALLEE 12
+
+# Saved-registers area (SRA).
+#define FP 16
+#define GP 20
+#define RA 24
+#define PADDING 28
+
+#  Argument-building area caller (ABA caller).
+#define M_A0 32
+#define N_A1 36
+
+
+#define SIZE_SF PADDING + 4
+
+
+# Debugger metadata.
+  .frame  fp,SIZE_SF,ra
+  
+# Allocate memory for the stack. 
+subu  sp,sp,SIZE_SF
+  
+# SRA beginning area.
+.cprestore GP
+
+# Save the callee-saved registers used by the caller in the SRA.
+sw  fp,FP(sp)
+sw  gp,GP(sp)
+# We must set the $fp to the beginning of the stack.
+move $fp,sp
+
+# Now we save the arguments that were loaded by the caller
+# in the area reserved by the caller.
+sw  a0,M_A0(fp)
+sw  a1,N_A1(fp)
+
+// mcd(unsigned int m, unsigned int n) 
+mcd:
+	# case if (m == 0)
+	lw	t0,M_A0(fp)
+	bnez	t0,case_n_0
+
+	# return m;
+	lw	v0,M_A0(fp)
+	b	stack_unwinding
+
+case_n_0:
+	# case  if (n == 0)
+	lw	t0,N_A1(fp)
+	bnez	t0,case_equal
+
+	# return n;
+	lw	v0,N_A1(fp)
+	b	stack_unwinding
+
+case_equal:
+	# case  if (m == n)
+	lw	t0,M_A0(fp)
+	lw	t1,N_A1(fp)
+	bne	t1,t0,m_high_n
+
+	# return m;
+	lw	v0,M_A0(fp)
+	b	stack_unwinding
+
+m_high_n:
+	# case if (m > n)
+	lw	t0,M_A0(fp)
+	lw	t1,N_A1(fp)
+	slt	t0,t0,t1
+	beqz	v0,other_case
+	
+	# return _mcd(m - n, n);
+	lw	t0,M_A0(fp)
+	lw	t1,N_A1(fp)
+	subu	t0,t0,t1
+	lw	a1,N_A1(fp)
+	move	a0,t0
+	jal      mcd
+
+other_case:	
+	# return _mcd(m, n - m);
+	lw	t0,M_A1(fp)
+	lw	t1,N_A1(fp)
+	subu	t0,t1,t0
+	move	a0,t0
+	move	a1,t1
+	jal      mcd
+	
+stack_unwinding:
+	lw        a1, (SIZE_SF + 4)(sp)
+	lw        a0, (SIZE_SF)(sp)
+  
+	lw        ra, RA(sp)
+	lw        fp, FP(sp)
+	lw        gp, GP(sp)
+	addu      sp, sp, SIZE_SF
+	jr        ra
+
+.end        mcd
+	
--- tp1/mcm.s
+++ tp1/mcm.s
@@ -1,36 +1,132 @@
-0000112c <_mcm>:
+#include <sys/regdef.h>
 
-// mínimo común múltiplo
-int _mcm(int m, int n) {
-lui	gp,0x2
-addiu	gp,gp,-28860
-addu	gp,gp,t9
-addiu	sp,sp,-32
-sw	ra,28(sp)
-sw	s8,24(sp)
-move	s8,sp
-sw	gp,16(sp)
-sw	a0,32(s8)
-sw	a1,36(s8)
-return (m / _mcm_rec(m, n)) * n;
-lw	a1,36(s8)
-lw	a0,32(s8)
-lw	v0,-32708(gp)
-move	t9,v0
-bal	10a4 <_mcm_rec>
-nop
-lw	gp,16(s8)
-move	v1,v0
-lw	v0,32(s8)
-teq	v1,zero,0x7
-div	zero,v0,v1
-mfhi	v0
-mflo	v1
-lw	v0,36(s8)
-mul	v0,v1,v0
-}
-move	sp,s8
-lw	ra,28(sp)
-lw	s8,24(sp)
-addiu	sp,sp,32
-jr	ra
+.text
+.abicalls
+.align  2
+.globl  mcm
+.ent  mcm
+
+# Pipeline magic.
+.set        noreorder
+.cpload     t9
+.set        reorder
+
+/* STACK DESIGN - SIZE: 32
+      ==========
+  36  |   a1   |
+      ---------- ABA CALLER (8)
+  32  |   a0   |
+      ==========
+  28  | PADDING|
+      ----------
+  24  |   ra   |
+      ---------- SRA (16)
+  20  |   gp   |
+      ----------
+  16  |   fp   |
+      ==========
+  12  |   a3   |
+      ----------
+  8   |   a2   |
+      ---------- ABA CALLEE(16)
+  4   |   a1   |
+      ----------
+  0   |   a0   |
+      ==========
+*/
+
+
+#  Argument-building area callee (ABA callee).
+#define A0_CALLEE 0
+#define A1_CALLEE 4
+#define A2_CALLEE 8
+#define A3_CALLEE 12
+
+# Saved-registers area (SRA).
+#define FP 16
+#define GP 20
+#define RA 24
+#define PADDING 28
+
+#  Argument-building area caller (ABA caller).
+#define M_A0 32
+#define N_A1 36
+
+
+#define SIZE_SF PADDING + 4
+
+
+# Debugger metadata.
+  .frame  fp,SIZE_SF,ra
+  
+# Allocate memory for the stack. 
+subu  sp,sp,SIZE_SF
+  
+# SRA beginning area.
+.cprestore GP
+
+# Save the callee-saved registers used by the caller in the SRA.
+sw  fp,FP(sp)
+sw  gp,GP(sp)
+# We must set the $fp to the beginning of the stack.
+move $fp,sp
+
+# Now we save the arguments that were loaded by the caller
+# in the area reserved by the caller.
+sw  a0,M_A0(fp)
+sw  a1,N_A1(fp)
+
+// _mcm_rec(unsigned int m, unsigned int n)
+_mcm_rec:
+	
+	# case if (m == 0)
+	lw	t0,M_A0(fp)
+	bnez	t0,mcm_rec_recursive
+
+	# return n
+	lw	v0,N_A1(fp)
+	b	stack_unwinding
+
+mcm_rec_recursive:
+	// return _mcm_rec(n % m, m);
+	lw	t0,M_A0(fp)
+	lw	t1,N_A0(fp)
+	div	t1,t0
+	mfhi	t2           # t2 = n % m
+	move	a1,t0
+	move	a0,t2
+	jal	_mcm_rec
+
+
+// mcm(unsigned int m, unsigned int n) 
+// return (m / _mcm_rec(m, n)) * n;
+mcm:
+	lw	t0,M_A0(fp) # t0 = m
+	lw	t1,N_A0(fp) # t1 = n
+	move 	a0,t0 	   # a0 = t0
+	move 	a1,t1       # a1 = t1
+	b	_mcm_rec
+	
+	move	t2,v0       # t2 =_mcm_rec(m, n)
+	lw	t0,M_A0(fp) # t0 = m
+	div	t0,t2       
+	mfhi	t3
+	mflo	t4 	   # t4 = m / _mcm_rec(m, n)
+	lw	t0,N_A0(fp) # t0 = n
+	mul	t0,t4,t0    # t0 = t4 * t0 
+	move 	v0,t0       # Return mcm value
+	b	stack_unwinding
+
+
+stack_unwinding:
+	lw        a1, (SIZE_SF + 4)(sp)
+	lw        a0, (SIZE_SF)(sp)
+  
+	lw        ra, RA(sp)
+	lw        fp, FP(sp)
+	lw        gp, GP(sp)
+	addu      sp, sp, SIZE_SF
+	jr        ra
+
+.end        mcm
+
